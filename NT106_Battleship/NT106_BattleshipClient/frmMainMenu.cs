9062000959094761ea12a1f9817c23e80af01ee6
Vo Minh Kiet <vominhkiet5018@gmail.com>
2025-10-26 01:44:22 +0700

    Code giao dien MainMenu va UserInfo nhung chua xong

--- NT106_Battleship/NT106_BattleshipClient/frmMainMenu.cs
+++ NT106_Battleship/NT106_BattleshipClient/frmMainMenu.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,41 @@ namespace NT106_BattleshipClient
         public frmMainMenu()
         {
             InitializeComponent();
+            
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+            // Lấy kích thước màn hình chính
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+
+            // Áp dụng kích thước đó cho Form
+            this.Size = screen.Size;
+            this.Location = new Point(0, 0); // Đặt Form ở góc trên bên trái
+
+            
+        }
+
+        private void btnHoSo_Click(object sender, EventArgs e)
+        {
+            frmUserInfo MoForm = new frmUserInfo();
+            MoForm.ShowDialog();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+        "Bạn có chắc chắn muốn thoát?",
+        "Xác nhận thoát",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Question
+    );
+
+            
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); 
+            }
         }
     }
 }
