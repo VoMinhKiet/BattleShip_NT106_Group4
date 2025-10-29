56a141135def9744b02208604092ad0bed1e7e0a
namle1602 <lenamkhongbocuoc7711@gmail.com>
2025-10-29 22:21:09 +0700

    Add form đăng nhập, đăng kí, quên mật khẩu, và tạo lại mật khẩu, 29/10/2025.

---
+++ NT106_Battleship/NT106_BattleshipClient/frmResetpassword.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NT106_BattleshipClient
+{
+    public partial class frmResetpassword : Form
+    {
+        public frmResetpassword()
+        {
+            InitializeComponent();
+            txtConfirmpassword.PasswordChar = '*';
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bntExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn thoát?",
+                "Xác nhận thoát",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void chkShowpassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkShowpassword.Checked)
+                txtConfirmpassword.PasswordChar = '\0';
+            else
+                txtConfirmpassword.PasswordChar = '*';
+        }
+
+        private void linkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmLogin f = new frmLogin();
+            f.Show();
+            this.Hide();
+        }
+
+        private void linkCreateAccount_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmSignup f = new frmSignup();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
