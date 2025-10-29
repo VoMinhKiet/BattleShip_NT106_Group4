bd334fd66e3ec5b99d148d496ab7cc0e0756024c
TuanDat135 <trantuandat130526@gmail.com>
2025-10-29 21:51:37 +0700

    feat: Thêm form xác nhận đầu hàng (frmSurrender)

--- NT106_Battleship/NT106_BattleshipClient/frmSurrender.Designer.cs
+++ NT106_Battleship/NT106_BattleshipClient/frmSurrender.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDauHang = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "XÁC NHẬN ĐẦU HÀNG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label2.Location = new System.Drawing.Point(5, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bạn có chắc muốn đầu hàng không?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDauHang
+            // 
+            this.btnDauHang.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDauHang.BackColor = System.Drawing.Color.Tomato;
+            this.btnDauHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDauHang.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDauHang.ForeColor = System.Drawing.Color.White;
+            this.btnDauHang.Location = new System.Drawing.Point(17, 98);
+            this.btnDauHang.Name = "btnDauHang";
+            this.btnDauHang.Size = new System.Drawing.Size(110, 40);
+            this.btnDauHang.TabIndex = 2;
+            this.btnDauHang.Text = "Đầu hàng";
+            this.btnDauHang.UseVisualStyleBackColor = false;
+            this.btnDauHang.Click += new System.EventHandler(this.btnDauHang_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHuy.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnHuy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHuy.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.ForeColor = System.Drawing.Color.White;
+            this.btnHuy.Location = new System.Drawing.Point(228, 98);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(110, 40);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = false;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // frmSurrender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmSurrender";
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(350, 150);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnDauHang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSurrender";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.frmSurrender_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDauHang;
+        private System.Windows.Forms.Button btnHuy;
     }
 }
--- NT106_Battleship/NT106_BattleshipClient/frmSurrender.cs
+++ NT106_Battleship/NT106_BattleshipClient/frmSurrender.cs
@@ -16,5 +16,21 @@ namespace NT106_BattleshipClient
         {
             InitializeComponent();
         }
+
+        private void btnDauHang_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close(); // Đóng form pop-up
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close(); // Đóng form pop-up
+        }
+        private void frmSurrender_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
