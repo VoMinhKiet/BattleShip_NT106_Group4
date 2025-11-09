5f248bbd491c955b5b73f0385ad9014a51eda4a6
Vo Minh Kiet <vominhkiet5018@gmail.com>
2025-11-09 19:43:50 +0700

    chỉnh UI

--- NT106_Battleship/NT106_BattleshipClient/frmLogin.Designer.cs
+++ NT106_Battleship/NT106_BattleshipClient/frmLogin.Designer.cs
@@ -213,6 +213,7 @@
             this.Controls.Add(this.pnlLogin);
             this.Name = "frmLogin";
             this.Text = "frmLogin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmLogin_Load);
             this.pnlLogin.ResumeLayout(false);
             this.pnlLogin.PerformLayout();
