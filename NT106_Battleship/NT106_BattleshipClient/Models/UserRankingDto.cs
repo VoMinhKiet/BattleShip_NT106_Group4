5298f24d5e07fe16c95c75df01a265d347e51734
namle1602 <lenamkhongbocuoc7711@gmail.com>
2025-12-28 09:33:46 +0700

    add cập nhật dữ liệu sau trận (thông tin số sao, số trận, tỉ lệ thắng)

---
+++ NT106_Battleship/NT106_BattleshipClient/Models/UserRankingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NT106_BattleshipClient.Models
+{
+    public class UserRankingDto
+    {
+        public int capSao { get; set; }
+        public int tongSoTran { get; set; }
+        public double tiLeThang { get; set; }
+    }
+}
